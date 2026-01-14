fefc63d5fb2ee7e6d152b4d9aec52da0ad2c02a7
limkyuseok <lgkk50025@gmail.com>
2026-01-14 21:44:59 +0900

    (2026. 01. 14 파일 입출력)

---
+++ porder/Source.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+#pragma region 파일 입출력
+
+#pragma region 파일 쓰기
+
+	// 첫 번째 매개 변수 (파일의 이름)
+	// 두 번째 매개 변수 (파일의 입출력 모드)
+
+	FILE* file = fopen("data.txt","w");
+	
+	fputs("Charater Information\n", file);
+	fputs("Health  :\n", file);
+	fputs("Attack  :\n", file);
+	fputs("Defense :\n", file);
+
+	fclose(file);
+
+#pragma endregion
+
+
+#pragma endregion
+
+	return 0;
+}
