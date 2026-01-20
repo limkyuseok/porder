acf28b755c3234bfc8ad76b125fa6fc68be163c9
limkyuseok <lgkk50025@gmail.com>
2026-01-20 21:43:40 +0900

    (2026. 01. 20 열거형)

--- porder/Source.c
+++ porder/Source.c
@@ -1,9 +1,65 @@
 #include <stdio.h>
+#include <conio.h>
+#include <windows.h>
 
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+void position(int x, int y)
+{
+	COORD position = { x,y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+
+}
 
 int main()
 {
-	printf("system ");
+	//position(5, 5);
+
+	//printf("¡Ú");
+
+	int x = 0;
+	int y = 0;
+
+	char key = 0;
+
+	while (1)
+	{
+
+		key = _getch();
+
+		if (key == -32 || key == 0)
+		{
+			key = _getch();
+		}
+
+			switch (key)
+			{
+			case UP: y--;
+				break;
+			case LEFT: x-=2;
+				break;
+			case RIGHT: x+=2;
+				break;
+			case DOWN: y++;
+				break;
+			default: printf("exception\n");
+				break;
+			}
+			system("cls");
+
+			position(x, y);
+
+			printf("¡Ú");
+
+	
+	}
+
+
+
 
 	return 0;
 }
